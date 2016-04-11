af699fc1f1f746fd6234783b5aa573fe1970f3f6
Satityr <satityr007@gmail.com>
2016-04-11 16:45:39 +0100

    still changing

--- src/com/inov/ss7analayser/testes/Test1ofmany.java
+++ src/com/inov/ss7analayser/testes/Test1ofmany.java
@@ -2,6 +2,28 @@ package com.inov.ss7analayser.testes;
 
 public class Test1ofmany {
 
-	String msg = "test ok !" ;
-	
+
+	public static void main(String[] args) {
+		try { 
+			doStuff(); 
+			System.out.println("1");
+		}	
+		catch( RuntimeException re ) {
+		System.out.println("2");
+		}
+		
+	}
+
+	public static void doStuff() {
+		if (false) {
+			
+			throw  new RuntimeException() ;
+			}
+		doMoreStuff();
+		System.out.println("3 ");
+	}
+
+	public static void doMoreStuff() {
+		System.out.println("4");
+	}
 }
--- src/com/inov/ss7analayser/testes/test2.java
+++ src/com/inov/ss7analayser/testes/test2.java
@@ -1,5 +1,15 @@
 package com.inov.ss7analayser.testes;
 
-public class test2 {
-	int y;
+
+	
+	public class test2 {
+		
+		int da(int a){
+			
+			System.out.println("int da(int a)");
+			return a;
+		}
+		
+		
+		
 }
