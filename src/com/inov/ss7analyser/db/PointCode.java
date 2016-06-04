3dcbc852e86559823f4ee161e13c606dc782975b
Satityr <Satityr@10.5.1.24>
2016-06-04 04:35:16 +0100

    implement singelton fot PointCode

--- src/com/inov/ss7analyser/db/PointCode.java
+++ src/com/inov/ss7analyser/db/PointCode.java
@@ -2,27 +2,64 @@ package com.inov.ss7analyser.db;
 
 
 
-//  USE SINGLETON PATTERN
+
+/**
+ * @author OUBIDAR Abderrahim 
+ *
+ */
+
+/* 
+ * 
+ *   this class will contain all the point codes stored in the database, also the ones we will get 
+ *   from analysed packets
+ *   
+ *   it uses the singelton design pattern to ensure working with one PointCode instance
+ *   
+ *   in ITU, PC is coded in 14 bits which gives a range from 0 to 16383
+ *   
+ *   pointCode[3637] equals true means that the PC:3637 exists in the DB
+ * 
+ */
 
 public class PointCode {
 	
 	
 	private final static int MAX_PC_VALUE = 16384 ;
 	
+	private static PointCode instance = null ;
+	
 	private boolean[] pointCode = new boolean[MAX_PC_VALUE];
 
-	public PointCode() {
-		super();
-
-		for(int i = 0 ; i < MAX_PC_VALUE ; i++ ){
-			pointCode[i] = false ;
+	private PointCode() { }
+	
+	public static PointCode getInstance(){
+		
+		if( instance == null ){
+			
+			synchronized(PointCode.class) {
+				
+				if( instance == null ){
+					
+					instance = new PointCode();
+					
+					for(int i = 0 ; i < MAX_PC_VALUE ; i++ ){
+						instance.pointCode[i] = false ;
+					}
+				}
+					
+			}
+			
 		}
+		
+		return instance;
 		
 	}
 	
+	
+	
 	public boolean isItNew(int pc){
 		
-		return pointCode[pc];
+		return instance.pointCode[pc];
 	}
 	
 	
@@ -30,20 +67,20 @@ public class PointCode {
 		
 		int i = (int) pc;
 		
-		return pointCode[i];
+		return instance.pointCode[i];
 		
 	}
 	
 	public void setPointCode(int pc){
 		
-		pointCode[pc] = true ;
+		instance.pointCode[pc] = true ;
 	}
 	
 	public void setPointCode(long pc){
 		
 		int i = (int) pc;
 		
-		pointCode[i] = true ;
+		instance.pointCode[i] = true ;
 	}
 	
 	
